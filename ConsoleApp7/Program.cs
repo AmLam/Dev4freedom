93d5d0bc8211bca4cb05ec4f5bf2c63f00a7cfc0
A M Lambers <annemargotlambers@gmail.com>
2017-12-13 17:58:58 +0100

    This should do the trick

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //get user input
+            ConsoleKeyInfo UserInput = Console.ReadKey();
+            char Input = UserInput.KeyChar;
+
+            //check if user input is letter. If so, proceed...
+            if (Char.IsLetter(Input))
+            {
+                if ((int)Input > 90)
+                {
+                    int Conv = ((((int)Input - 97) + 1) % 26) + 97;
+                    Console.WriteLine();
+                    Console.WriteLine("{0}", (char)Conv);
+                    Console.ReadKey();
+                }
+                else
+                {
+                    int Conv = ((((int)Input - 65) + 1) % 26) + 65;
+                    Console.WriteLine();
+                    Console.WriteLine("{0}", (char)Conv);
+                    Console.ReadKey();
+                }
+            }
+            //If not, show error message
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine("Error: Must enter letter!");
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
